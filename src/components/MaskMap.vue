c597241beed1554e201b040d50bc143a3fee4e3d
Roger <Worth22Focus3000@gmail.com>
2021-10-18 22:22:48 +0800

    將部分邏輯抽到模組檔案做管理

--- src/components/MaskMap.vue
+++ src/components/MaskMap.vue
@@ -4,108 +4,110 @@ import { useStore } from "vuex";
 import L from "leaflet";
 import "leaflet.markercluster";
 
-import { iconFilter } from "../plugins/leaflet";
+import { createPopup, iconFilter } from "../plugins/leaflet";
 
 export default {
   setup() {
     const store = useStore();
-    let geoJSONLayer = {};
+    let markerData;
+    let userPositionSave = false;
 
     /**
-     * 觸發 領口罩的藥局分布資料 API
+     * 取得 藥局資料
      */
-    const getMaskStores = () => {
+    const getMaskStores = map => {
       store.dispatch("getMaskStores").then(res => {
         if (res.status === 200) {
-          setInitMaskStores();
+          initMap(map, res.data.features);
         }
       });
     };
 
     /**
-     * 初始化地圖 (預設經緯度地點)
+     * 初始化地圖的設定
      */
-    const setInitMaskStores = () => {
-      store.dispatch("searchMaskStores", "臺北市");
-    };
-
-    /**
-     * 設定地圖
-     */
-    const setMaskStores = (data, map) => {
-      map.setView([25.052043, 121.5552104], 18);
-
+    const initMap = (map, data) => {
       L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(
         map
-      );
+      ); // 圖層來源
+      map.setView([25.052137, 121.555235], 13); // 地圖初始 位置&縮放大小
+      let markers = new L.MarkerClusterGroup({
+        disableClusteringAtZoom: 16
+      }).addTo(map); // 將圖標彙整成群組型態
 
-      var markers = new L.MarkerClusterGroup().addTo(map);
-
+      // 創建每一個圖標&圖標模板
       for (let i = 0; data.length > i; i++) {
+        const lat = data[i].geometry.coordinates[1];
+        const lng = data[i].geometry.coordinates[0];
+
         markers.addLayer(
-          L.marker(
-            [data[i].geometry.coordinates[1], data[i].geometry.coordinates[0]],
-            { icon: iconFilter(data[i]) }
-          ).bindPopup(
-            `
-              <h3 class="fs-4 fw-bold mb-2">${data[i].properties.name}</h3>
-              <p class="fs-8 m-0 mb-1">地址 | ${data[i].properties.address}</p>
-              <p class="fs-8 m-0 mb-1">連絡電話 | ${data[i].properties.phone}</p>
-              <p class="fs-8 m-0 mb-1">備註 | ${data[i].properties.note}</p>
-              <p class="fs-8 m-0 mb-2 text-gray-500">資訊更新於 ${data[i].properties.updated} 天前</p>
-              <div class="d-flex mb-2 justify-content-between">
-                <button type="button" class="btn btn-secondary me-1 text-white w-47.5">成人口罩 ${data[i].properties.mask_adult} 個</button>
-                <button type="button" class="btn btn-secondary text-white w-47.5">兒童口罩 ${data[i].properties.mask_child} 個</button>
-              </div>
-              <a href="https://www.google.com/search?q=${data[i].properties.name}" target="_blank"
-                class="btn btn-primary text-white w-100 mb-2"> Google 關鍵字搜尋
-              <a href="https://www.google.com/maps/search/${data[i].properties.name}/@${data[i].geometry.coordinates[1]},${data[i].geometry.coordinates[0]}" target="_blank"
-                class="btn btn-success text-white w-100"> Google Map 路線導航
-              `
+          L.marker([lat, lng], { icon: iconFilter(data[i]) }).bindPopup(
+            createPopup(data[i])
           )
         );
       }
-      geoJSONLayer = markers;
+      markerData = markers;
+      // 將圖標掛載在圖層上
       map.addLayer(markers);
+
+      // 預設經緯度地點
+      store.dispatch("searchMaskStores", "臺北市");
     };
 
     onMounted(() => {
-      getMaskStores();
+      const map = L.map("map"); // 建立地圖、綁定目標DOM元素
+      getMaskStores(map);
 
-      const map = L.map("map");
-
-      const data = computed(() => {
-        return store.getters.getOriginMaskStores;
-      });
-
-      watch(data, (nVal, oVal) => {
-        if (nVal !== oVal) {
-          setMaskStores(data.value, map);
-        }
-        return;
-      });
-
-      const data2 = computed(() => {
+      // 取得 座標
+      const coordinates = computed(() => {
         return store.getters.getCoordinates;
       });
 
-      watch(data2, (oVal, nVal) => {
-        if (oVal !== nVal) {
-          map.flyTo(data2.value, 18);
+      // 取得 座標來源
+      const source = computed(() => {
+        return store.getters.getCoordinatesSource;
+      });
 
+      watch([source, coordinates], (oVal, nVal) => {
+        if (oVal === nVal) return;
+        map.flyTo(coordinates.value, 16);
+
+        // 藥局位置
+        if (source.value === "store") {
           map.once("moveend", () => {
-            geoJSONLayer.eachLayer(layer => {
-              if (
-                layer._latlng.lat === data2.value[0] &&
-                layer._latlng.lng === data2.value[1]
+            markerData.eachLayer(layer => {
+              const lat = layer._latlng.lat;
+              const lng = layer._latlng.lng;
+              if (lat === coordinates.value[0] && lng === coordinates.value[1])
+                layer.openPopup();
+            });
+          });
+          return;
+        }
+
+        // 使用者位置
+        if (source.value === "user") {
+          if (!userPositionSave) {
+            markerData
+              .addLayer(
+                L.marker(coordinates.value, { icon: iconFilter() }).bindPopup(
+                  "現在位置"
+                )
               )
+              .openPopup();
+            userPositionSave = true;
+          }
+          markerData.eachLayer(layer => {
+            const lat = layer._latlng.lat;
+            const lng = layer._latlng.lng;
+            map.once("moveend", () => {
+              if (lat === coordinates.value[0] && lng === coordinates.value[1])
                 layer.openPopup();
             });
           });
         }
       });
     });
-
     return {};
   }
 };
