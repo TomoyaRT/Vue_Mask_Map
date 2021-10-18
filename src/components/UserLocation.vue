99a1063b139822169552f10a48bd456f1b399c17
Roger <Worth22Focus3000@gmail.com>
2021-10-18 22:21:08 +0800

    新增座標的來源

--- src/components/UserLocation.vue
+++ src/components/UserLocation.vue
@@ -14,6 +14,7 @@ export default {
             const lng = position.coords.longitude;
 
             store.dispatch("setCoordinates", [lat, lng]);
+            store.dispatch("setCoordinatesSource", "user");
           });
         }
         else
